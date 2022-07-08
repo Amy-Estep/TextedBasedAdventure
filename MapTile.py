255e5f45f37a9e40c36579fb017d854aecf7f90d
Amy-Estep <amy@estephome.co.uk>
2022-07-08 20:49:07 +0100

    many more subclasses added and run game methods added as well as sorting out importing

--- MapTile.py
+++ MapTile.py
@@ -1,3 +1,5 @@
+import item, enemy, action, world
+
 class MapTile:
     def __init__(self, x, y):
         self.x = x
@@ -49,6 +51,12 @@ class EnemyRoom(MapTile):
             the_player.hp = the_player.hp - self.enemy.damage
             print("Enemy does {} damage. You have {} HP remaining.".format(self.enemy.damage, the_player.hp))
 
+    def available_actions(self):
+        if self.enemy.is_alive():
+            return [actions.Flee(tile=self), actions.Attack(enemy=self.enemy)]
+        else:
+            return self.adjacent_moves()
+
 class EmptyCavePath(MapTile):
     def intro_text(self):
         return """
@@ -59,4 +67,34 @@ class EmptyCavePath(MapTile):
         #Nothing currently implemented
         pass
  
+def adjacent_moves(self):
+    """Returns all move actions for adjacent tiles."""
+    moves = []
+    if world.tile_exists(self.x + 1, self.y):
+        moves.append(actions.MoveEast())
+    if world.tile_exists(self.x - 1, self.y):
+        moves.append(actions.MoveWest())
+    if world.tile_exists(self.x, self.y - 1):
+        moves.append(actions.MoveNorth())
+    if world.tile_exists(self.x, self.y + 1):
+        moves.append(actions.MoveSouth())
+    return moves
+ 
+def available_actions(self):
+    """Returns all of the available actions in this room."""
+    moves = self.adjacent_moves()
+    moves.append(actions.ViewInventory())
+ 
+    return moves
+
+class LeaveCaveRoom(MapTile):
+    def intro_text(self):
+        return """
+        You see a light at the end of the tunnel...
+
+        Victory is yours!
+        """
+ 
+    def modify_player(self, player):
+        player.victory = True
 
